c17eb962893a7bbd54cfe5f11a088c0b49f4139c
Angelo Pirola <angelo9871@gmail.com>
2023-07-14 09:11:34 +0200

    Primo commit libreria EFCore Gestione Sagre
    closes #10

---
+++ src/GestioneSagre.EFCore/Extensions/RegisterServices.cs
@@ -0,0 +1,34 @@
+﻿namespace GestioneSagre.EFCore.Extensions;
+
+public static class RegisterServices
+{
+    public static IServiceCollection AddDbContextSQLServer<TDbContext>(this IServiceCollection services, string connectionString) where TDbContext : DbContext
+    {
+        services.AddDbContext<TDbContext>(optionBuilder =>
+        {
+            optionBuilder.UseSqlServer(connectionString, options =>
+            {
+                // Abilito il connection resiliency (Provider di SQL Server è soggetto a errori transienti)
+                // Info su: https://docs.microsoft.com/en-us/ef/core/miscellaneous/connection-resiliency
+                options.EnableRetryOnFailure(3);
+                options.MigrationsAssembly(typeof(TDbContext).Assembly.FullName);
+            });
+        });
+
+        return services;
+    }
+
+    public static ConnectionStringsViewModel GetConnectionString(IConfigurationSection settings, ConnectionStringsInputModel inputModel)
+    {
+        inputModel.ConnectionString = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") switch
+        {
+            "Development" => settings["Default"],
+            _ => settings["Docker"]
+                .Replace("NOME-DOCKER", settings["DockerHost"])
+                .Replace("USERNAME", settings["Username"])
+                .Replace("PASSWORD", settings["Password"])
+        };
+
+        return new ConnectionStringsViewModel() { ConnectionString = inputModel.ConnectionString };
+    }
+}
---
+++ src/GestioneSagre.EFCore/GenericRepository/Interfaces/IRepository.cs
@@ -0,0 +1,23 @@
+﻿namespace GestioneSagre.EFCore.GenericRepository.Interfaces;
+
+public interface IRepository<TEntity> where TEntity : class, new()
+{
+    Task<List<TEntity>> GetItemsAsync(Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>> includes,
+        Expression<Func<TEntity, bool>> condition, CancellationToken cancellationToken = default);
+
+    Task<List<TEntity>> GetOrderedItemsAsync(Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>> includes,
+        Expression<Func<TEntity, bool>> conditionWhere, Expression<Func<TEntity, dynamic>> orderBy,
+        OrderType orderType = OrderType.Ascending, CancellationToken cancellationToken = default);
+
+    Task<TEntity> GetItemByIdAsync(Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>> includes,
+        Expression<Func<TEntity, bool>> condition, CancellationToken cancellationToken = default);
+
+    Task<int> GetItemsCountAsync(Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>> includes,
+        Expression<Func<TEntity, bool>> condition, CancellationToken cancellationToken = default);
+
+    Task CreateAsync(TEntity entity, CancellationToken cancellationToken);
+
+    Task UpdateAsync(TEntity entity, CancellationToken cancellationToken);
+
+    Task DeleteAsync(TEntity entity, CancellationToken cancellationToken);
+}
