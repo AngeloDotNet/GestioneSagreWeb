9c1adb4b9e193318e3d28fc880846d52febe2a3c
Angelo Pirola <angelo9871@gmail.com>
2023-09-04 09:07:05 +0200

    Aggiornato BL Gateway

--- src/Gateway/GestioneSagre.Gateway.BusinessLayer/Extensions/RegisterServices.cs
+++ src/Gateway/GestioneSagre.Gateway.BusinessLayer/Extensions/RegisterServices.cs
@@ -10,14 +10,20 @@ public static class RegisterServices
     public static IServiceCollection AddRegisterConfigureServices(this IServiceCollection services, IConfiguration configuration, string serviceName, string swaggerName)
     {
         var customServices = configuration.GetSection("PointAPI");
+        var serverService = configuration.GetSection("Servers");
+
+        var connStringRedis = serverService["Redis-Host"] + ":" + serverService["Redis-Port"];
+        var connStringSeq = serverService["Seq-Host"];
+        var SeqApiKey = serverService["Seq-ApiKey"];
+        var tagsAPI = new[] { "web api", "swagger" };
+        var tagsRedis = new string[] { "infrastructure", "redis" };
 
         services.AddPolicyCors(serviceName);
         services.AddSerilogSeqServices();
 
         services.AddSwaggerGenConfig($"{swaggerName}", "v1");
-        services.AddHealthChecks()
-            .AddProcessAllocatedMemoryHealthCheck(100, name: "Allocated Memory")
-            .AddUrlGroup(new Uri(customServices["Url"]), customServices["Name"], HealthStatus.Degraded, new[] { "web api", "swagger" });
+        services.AddHealthChecks().AddRedis(connStringRedis, "Redis Check", HealthStatus.Degraded, tagsRedis);
+        services.AddCustomHealthChecks(customServices["Url"], customServices["Name"], tagsAPI, connStringSeq, SeqApiKey);
 
         return services;
     }
