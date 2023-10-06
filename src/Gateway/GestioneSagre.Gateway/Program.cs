ca28be45a6266242255f07d7b779fa490cee7cfd
Angelo Pirola <angelo9871@gmail.com>
2023-10-06 09:18:08 +0200

    Refactoring

--- src/Gateway/GestioneSagre.Gateway/Program.cs
+++ src/Gateway/GestioneSagre.Gateway/Program.cs
@@ -9,7 +9,12 @@ public class Program
         var builder = WebApplication.CreateBuilder(args);
 
         builder.AddSerilogOptionsBuilder();
-        builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+        builder.Configuration
+            .AddJsonFile("ocelot.json", optional: false, reloadOnChange: true)
+            .AddJsonFile("ocelot-categorie.json", optional: false, reloadOnChange: true)
+            .AddJsonFile("ocelot-dataprotection.json", optional: false, reloadOnChange: true)
+            .AddJsonFile("ocelot-prodotti.json", optional: false, reloadOnChange: true)
+            .AddJsonFile("ocelot-utility.json", optional: false, reloadOnChange: true);
 
         Startup startup = new(builder.Configuration);
 
