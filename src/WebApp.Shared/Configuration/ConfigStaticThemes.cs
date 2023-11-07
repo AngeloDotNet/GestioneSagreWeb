a8f0d2d404c54389ef2d752f4e5ebbced11176ec
Angelo Pirola <angelo9871@gmail.com>
2023-11-08 00:14:56 +0100

    FE - Creata libreria shared

---
+++ src/WebApp.Shared/Configuration/ConfigStaticThemes.cs
@@ -0,0 +1,37 @@
+﻿using MudBlazor;
+
+namespace GestioneSagre.Web.Shared.Configuration;
+
+public static class ConfigStaticThemes
+{
+    public static MudTheme appliedTheme = new();
+
+    public static MudTheme SetChosenTheme(string theme)
+    {
+        appliedTheme = theme switch
+        {
+            //"Default" => GenerateDefaultTheme(),
+            //"Purple" => GeneratePurpleTheme(),
+            _ => GenerateDefaultTheme(),
+        };
+        return appliedTheme;
+    }
+
+    private static MudTheme GenerateDefaultTheme() =>
+        new MudTheme
+        {
+            Palette = new()
+            {
+                AppbarBackground = Colors.Grey.Default
+            }
+        };
+
+    //private static MudTheme GeneratePurpleTheme() =>
+    //    new MudTheme
+    //    {
+    //        Palette = new()
+    //        {
+    //            AppbarBackground = Colors.Purple.Default
+    //        }
+    //    };
+}
---
+++ src/WebApp.Shared/Configuration/FrontendParameters.cs
@@ -0,0 +1,13 @@
+﻿namespace GestioneSagre.Web.Shared.Configuration;
+
+public static class FrontendParameters
+{
+    // GET methods
+    public const string ENDPOINT_GET_HELLOWORLD = "http://sagre-api.aepserver.it/helloworld/";
+
+    // POST methods
+
+    // PUT methods
+
+    // DELETE methods
+}
