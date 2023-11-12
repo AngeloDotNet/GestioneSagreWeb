3fe21ced081365fc29df05c952b7b543b48af515
Angelo Pirola <angelo9871@gmail.com>
2023-11-12 10:06:13 +0100

    Aggiornato modulo Dashboard

---
+++ src/ModuliWeb/GestioneSagre.Modules.Dashboard/Pages/About.razor.cs
@@ -0,0 +1,24 @@
+﻿using MudBlazor;
+
+namespace GestioneSagre.Modules.Dashboard.Pages;
+
+public partial class About
+{
+    protected override async Task OnInitializedAsync() => await base.OnInitializedAsync();
+
+    private readonly List<BreadcrumbItem> items = new List<BreadcrumbItem>
+    {
+        new BreadcrumbItem("Home Page", href: "/"),
+        new BreadcrumbItem("About", href: null, disabled: true)
+    };
+
+    private static string YearCopyright => CalculateCurrentYear();
+    private static string CalculateCurrentYear()
+    {
+        var startYear = 2023;
+        var currentYear = DateTime.Now.Year;
+        var years = startYear == currentYear ? $"{startYear}" : $"{startYear} - {currentYear}";
+
+        return years;
+    }
+}
