807ce0e3f1b481d7c9b1a18ee0aaa20f5636569e
Angelo Pirola <angelo9871@gmail.com>
2023-11-12 10:07:30 +0100

    Aggiunta libreria Shared per la webapp

---
+++ src/WebApp.Shared/Components/EditorForm.razor.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using MudBlazor;
+
+namespace GestioneSagre.Web.Shared.Components;
+
+public partial class EditorForm<TContent>
+{
+    [Parameter] public TContent Model { get; set; } = default!;
+    [Parameter] public RenderFragment<TContent> ContentInfo { get; set; } = default!;
+    [Parameter] public EventCallback<TContent> OnSave { get; set; }
+    [Parameter] public EventCallback OnCancel { get; set; }
+
+    [Inject] public IDialogService Dialog { get; set; } = default!;
+
+    private EditContext editContext;
+
+    protected override void OnParametersSet()
+    {
+        editContext = new EditContext(Model);
+    }
+
+    async Task SubmitAsync() => await OnSave.InvokeAsync(Model);
+    async Task CancelAsync() => await OnCancel.InvokeAsync();
+}
