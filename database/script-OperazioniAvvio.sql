21f3de4e74048abd825f677a4303cba9ac76bd7d
Angelo Pirola <angelo9871@gmail.com>
2023-11-14 08:48:47 +0100

    Aggiunto script init database SQL Server

--- database/script-OperazioniAvvio.sql
+++ database/script-OperazioniAvvio.sql
@@ -50,7 +50,35 @@ CREATE INDEX [IX_Intestazioni_IdFesta] ON [Intestazioni] ([IdFesta]);
 GO
 
 INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
-VALUES (N'20231013071526_InitialMigration', N'7.0.12');
+VALUES (N'20231013071526_InitialMigration', N'7.0.13');
+GO
+
+COMMIT;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+DECLARE @var0 sysname;
+SELECT @var0 = [d].[name]
+FROM [sys].[default_constraints] [d]
+INNER JOIN [sys].[columns] [c] ON [d].[parent_column_id] = [c].[column_id] AND [d].[parent_object_id] = [c].[object_id]
+WHERE ([d].[parent_object_id] = OBJECT_ID(N'[Feste]') AND [c].[name] = N'DataInizio');
+IF @var0 IS NOT NULL EXEC(N'ALTER TABLE [Feste] DROP CONSTRAINT [' + @var0 + '];');
+ALTER TABLE [Feste] ALTER COLUMN [DataInizio] datetime2 NOT NULL;
+GO
+
+DECLARE @var1 sysname;
+SELECT @var1 = [d].[name]
+FROM [sys].[default_constraints] [d]
+INNER JOIN [sys].[columns] [c] ON [d].[parent_column_id] = [c].[column_id] AND [d].[parent_object_id] = [c].[object_id]
+WHERE ([d].[parent_object_id] = OBJECT_ID(N'[Feste]') AND [c].[name] = N'DataFine');
+IF @var1 IS NOT NULL EXEC(N'ALTER TABLE [Feste] DROP CONSTRAINT [' + @var1 + '];');
+ALTER TABLE [Feste] ALTER COLUMN [DataFine] datetime2 NOT NULL;
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20231108183037_RemoveDateTimeOnly', N'7.0.13');
 GO
 
 COMMIT;
