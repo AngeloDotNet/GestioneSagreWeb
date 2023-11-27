6601c125f98112e732e44aa304a0f6eb66d5053c
Angelo Pirola <angelo9871@gmail.com>
2023-11-28 00:03:24 +0100

    Refactoring Modulo Dashboard

--- src/ModuliWeb/GestioneSagre.Modules.Dashboard/Pages/About.razor.cs
+++ src/ModuliWeb/GestioneSagre.Modules.Dashboard/Pages/About.razor.cs
@@ -6,13 +6,13 @@ public partial class About
 {
     protected override async Task OnInitializedAsync() => await base.OnInitializedAsync();
 
-    private readonly List<BreadcrumbItem> items = new List<BreadcrumbItem>
+    private readonly List<BreadcrumbItem> items = new()
     {
         new BreadcrumbItem("Home Page", href: "/"),
         new BreadcrumbItem("About", href: null, disabled: true)
     };
 
-    public static string years = string.Empty;
+    private static string years = string.Empty;
     private static string YearCopyright => CalculateCurrentYear(2023, DateTime.Now.Year);
     private static string CalculateCurrentYear(int startYear, int actualYear)
         => years = startYear == actualYear ? $"{startYear}" : $"{startYear} - {actualYear}";
