dea2886f4d731435e80534b8756101018fcb2c31
Angelo Pirola <angelo9871@gmail.com>
2023-12-13 12:31:38 +0100

    Rimosso modulo Configurazione Iniziale

--- src/ModuliWeb/GestioneSagre.Modules.ConfigurazioneIniziale/GlobalUsings.cs
+++
@@ -1,5 +0,0 @@
-﻿global using GestioneSagre.Web.Models.ConfigurazioneIniziale;
-global using GestioneSagre.Web.Models.ConfigurazioneIniziale.InputModels;
-global using GestioneSagre.Web.Services.ConfigurazioneIniziale;
-global using Microsoft.AspNetCore.Components;
-global using MudBlazor;
