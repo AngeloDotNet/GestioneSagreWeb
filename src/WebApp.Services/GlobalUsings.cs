3d5a5e960c67cf5ef222da822909df4d5b14216f
Angelo Pirola <angelo9871@gmail.com>
2023-12-25 17:46:33 +0100

    Rimossa libreria WebApp Services

--- src/WebApp.Services/GlobalUsings.cs
+++
@@ -1,5 +0,0 @@
-﻿global using System.Net.Http.Json;
-global using GestioneSagre.Web.Models;
-global using GestioneSagre.Web.Models.ConfigurazioneIniziale;
-global using GestioneSagre.Web.Models.ConfigurazioneIniziale.InputModels;
-global using GestioneSagre.Web.Models.ConfigurazioneIniziale.ViewModels;
