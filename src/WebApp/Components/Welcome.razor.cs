2f83c71f4b43c882e42db85c22ab861b5ffc0982
Angelo Pirola <angelo9871@gmail.com>
2023-12-25 17:46:46 +0100

    Rimossa libreria WebApp

--- src/WebApp/Components/Welcome.razor.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace GestioneSagre.Web.App.Components;
-
-public partial class Welcome
-{
-    [Parameter] public string Version { get; set; } = string.Empty;
-    [Parameter] public string Channel { get; set; } = string.Empty;
-}
