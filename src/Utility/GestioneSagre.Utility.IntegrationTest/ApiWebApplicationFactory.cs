d712e9d5f2ddcf1a08649f2646fd0256ed38f149
Angelo Pirola <angelo9871@gmail.com>
2023-12-29 16:10:58 +0100

    Integration Test Utility

---
+++ src/Utility/GestioneSagre.Utility.IntegrationTest/ApiWebApplicationFactory.cs
@@ -0,0 +1,27 @@
+﻿using GestioneSagre.Utility.DataAccessLayer;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GestioneSagre.Utility.IntegrationTest;
+
+public class ApiWebApplicationFactory : WebApplicationFactory<Startup>
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        base.ConfigureWebHost(builder);
+
+        builder.ConfigureServices(services =>
+        {
+            var descriptor = services.SingleOrDefault(d => d.ServiceType == typeof(DbContextOptions<UtilityDbContext>));
+
+            if (descriptor != null)
+            {
+                services.Remove(descriptor);
+            }
+
+            services.AddDbContext<UtilityDbContext>(options => options.UseInMemoryDatabase("Utility-InMemory-Integration-Test"));
+        });
+    }
+}
---
+++ src/Utility/GestioneSagre.Utility.IntegrationTest/IntegrationTest.cs
@@ -0,0 +1,102 @@
+﻿using System.Net;
+using System.Net.Http.Json;
+using GestioneSagre.Utility.DataAccessLayer;
+using GestioneSagre.Utility.Shared.Models.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace GestioneSagre.Utility.IntegrationTest;
+
+public class IntegrationTest
+{
+    [Fact]
+    public async Task GetTipoClienteShouldResponseListNotNullAsync()
+    {
+        using var app = new ApiWebApplicationFactory();
+
+        var httpClient = app.CreateClient();
+        var response = await httpClient.GetAsync("/api/utility/tipocliente");
+        var items = await response.Content.ReadFromJsonAsync<List<ClienteTipoViewModel>>();
+
+        using var scope = app.Services.CreateScope();
+        using var context = scope.ServiceProvider.GetRequiredService<UtilityDbContext>();
+
+        Assert.NotNull(response);
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        Assert.NotNull(items);
+        Assert.NotEmpty(items);
+    }
+
+    [Fact]
+    public async Task GetTipoPagamentoShouldResponseListNotNullAsync()
+    {
+        using var app = new ApiWebApplicationFactory();
+
+        var httpClient = app.CreateClient();
+        var response = await httpClient.GetAsync("/api/utility/tipopagamento");
+        var items = await response.Content.ReadFromJsonAsync<List<PagamentoTipoViewModel>>();
+
+        using var scope = app.Services.CreateScope();
+        using var context = scope.ServiceProvider.GetRequiredService<UtilityDbContext>();
+
+        Assert.NotNull(response);
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        Assert.NotNull(items);
+        Assert.NotEmpty(items);
+        Assert.Single(items);
+    }
+
+    [Fact]
+    public async Task GetStatoScontrinoShouldResponseListNotNullAsync()
+    {
+        using var app = new ApiWebApplicationFactory();
+
+        var httpClient = app.CreateClient();
+        var response = await httpClient.GetAsync("/api/utility/statoscontrino");
+        var items = await response.Content.ReadFromJsonAsync<List<ScontrinoStatoViewModel>>();
+
+        using var scope = app.Services.CreateScope();
+        using var context = scope.ServiceProvider.GetRequiredService<UtilityDbContext>();
+
+        Assert.NotNull(response);
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        Assert.NotNull(items);
+        Assert.NotEmpty(items);
+    }
+
+    [Fact]
+    public async Task GetTipoScontrinoShouldResponseListNotNullAsync()
+    {
+        using var app = new ApiWebApplicationFactory();
+
+        var httpClient = app.CreateClient();
+        var response = await httpClient.GetAsync("/api/utility/tiposcontrino");
+        var items = await response.Content.ReadFromJsonAsync<List<ScontrinoTipoViewModel>>();
+
+        using var scope = app.Services.CreateScope();
+        using var context = scope.ServiceProvider.GetRequiredService<UtilityDbContext>();
+
+        Assert.NotNull(response);
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        Assert.NotNull(items);
+        Assert.NotEmpty(items);
+    }
+
+    [Fact]
+    public async Task GetValuteMonetarieShouldResponseListNotNullAsync()
+    {
+        using var app = new ApiWebApplicationFactory();
+
+        var httpClient = app.CreateClient();
+        var response = await httpClient.GetAsync("/api/utility/valutemonetarie");
+        var items = await response.Content.ReadFromJsonAsync<List<ValutaViewModel>>();
+
+        using var scope = app.Services.CreateScope();
+        using var context = scope.ServiceProvider.GetRequiredService<UtilityDbContext>();
+
+        Assert.NotNull(response);
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        Assert.NotNull(items);
+        Assert.NotEmpty(items);
+    }
+}
