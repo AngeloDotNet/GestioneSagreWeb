b39524825aa412a91d520088f043094ceb9e86f0
Angelo Pirola <angelo9871@gmail.com>
2024-01-02 16:13:48 +0100

    Aggiunto progetto ClassLibrary Prodotti Shared

---
+++ src/Prodotti/GestioneSagre.Prodotti.Shared/Models/InputModels/ProdottoCreateInputModel.cs
@@ -0,0 +1,18 @@
+﻿using GestioneSagre.Prodotti.Shared.Models.ValueObjects;
+
+namespace GestioneSagre.Prodotti.Shared.Models.InputModels;
+
+public class ProdottoCreateInputModel
+{
+    public Guid Id { get; set; }
+    public Guid IdFesta { get; set; }
+    public string Descrizione { get; set; }
+    public bool ProdottoAttivo { get; set; }
+    public Guid IdCategoria { get; set; }
+    public Price Prezzo { get; set; }
+    public int Quantita { get; set; }
+    public bool QuantitaAttiva { get; set; }
+    public int QuantitaScorta { get; set; }
+    public bool AvvisoScorta { get; set; }
+    public bool Prenotazione { get; set; }
+}
---
+++ src/Prodotti/GestioneSagre.Prodotti.Shared/Models/ValueObjects/Price.cs
@@ -0,0 +1,13 @@
+﻿namespace GestioneSagre.Prodotti.Shared.Models.ValueObjects;
+
+public class Price
+{
+    public string Currency { get; set; }
+    public decimal Amount { get; set; }
+
+    //public override string ToString() => $"{Currency} {Amount}";
+    public override string ToString()
+    {
+        return $"{Currency} {Amount:0.00}";
+    }
+}
