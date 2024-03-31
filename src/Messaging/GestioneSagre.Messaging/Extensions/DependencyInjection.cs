7b9acac62fb745e681ad4c39d217584279557d4e
Angelo Pirola <angelo9871@gmail.com>
2024-03-31 12:25:42 +0200

    Refactoring Messaging

---
+++ src/Messaging/GestioneSagre.Messaging/Extensions/DependencyInjection.cs
@@ -0,0 +1,71 @@
+﻿using GestioneSagre.Messaging.Models.Options;
+using MassTransit;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GestioneSagre.Messaging.Extensions;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddRabbitMQSender<Request>(this IServiceCollection services, RabbitOptions rabbitOptions)
+    {
+        services.AddMassTransit(x =>
+        {
+            x.UsingRabbitMq((context, cfg) =>
+            {
+                //cfg.QueueExpiration = TimeSpan.FromSeconds(rabbitOptions.QueueExpiration);
+                cfg.QueueExpiration = TimeSpan.FromSeconds(RabbitStaticConfig.queueExpiration);
+
+                cfg.Host(rabbitOptions.Host, rabbitOptions.VirtualHost, h =>
+                {
+                    h.Username(rabbitOptions.Username);
+                    h.Password(rabbitOptions.Password);
+                });
+
+                cfg.ReceiveEndpoint(rabbitOptions.ReceivedEndpoint, e =>
+                {
+                    e.Durable = RabbitStaticConfig.durable; //rabbitOptions.Durable;
+                    e.AutoDelete = RabbitStaticConfig.autoDelete; //rabbitOptions.AutoDelete;
+                    e.ExchangeType = RabbitStaticConfig.exchangeType; //rabbitOptions.ExchangeType;
+                    e.PrefetchCount = RabbitStaticConfig.prefetchCount; //rabbitOptions.PrefetchCount;
+
+                    //e.UseMessageRetry(r => r.Interval(rabbitOptions.RetryCount, rabbitOptions.RetryInterval));
+                    e.UseMessageRetry(r => r.Interval(RabbitStaticConfig.retryCount, RabbitStaticConfig.interval));
+                });
+            });
+
+            x.AddRequestClient(typeof(Request));
+        });
+
+        return services;
+    }
+
+    public static IServiceCollection AddRabbitMQReceiver<Consumer>(this IServiceCollection services, RabbitOptions rabbitOptions)
+    {
+        services.AddMassTransit(x =>
+        {
+            x.AddConsumers(typeof(Consumer).Assembly);
+            x.SetKebabCaseEndpointNameFormatter();
+
+            x.UsingRabbitMq((context, cfg) =>
+            {
+                cfg.Host(rabbitOptions.Host, rabbitOptions.VirtualHost, h =>
+                {
+                    h.Username(rabbitOptions.Username);
+                    h.Password(rabbitOptions.Password);
+                });
+
+                cfg.ReceiveEndpoint(rabbitOptions.ReceivedEndpoint, e =>
+                {
+                    e.Durable = RabbitStaticConfig.durable; //rabbitOptions.Durable;
+                    e.AutoDelete = RabbitStaticConfig.autoDelete; //rabbitOptions.AutoDelete;
+                    e.ExchangeType = RabbitStaticConfig.exchangeType; //rabbitOptions.ExchangeType;
+                    e.PrefetchCount = RabbitStaticConfig.prefetchCount; //rabbitOptions.PrefetchCount;
+
+                    e.ConfigureConsumers(context);
+                });
+            });
+        });
+
+        return services;
+    }
+}
---
+++ src/Messaging/GestioneSagre.Messaging/Extensions/RabbitStaticConfig.cs
@@ -0,0 +1,14 @@
+﻿namespace GestioneSagre.Messaging.Extensions;
+
+internal static class RabbitStaticConfig
+{
+    internal static bool durable = true;
+    internal static bool autoDelete = false;
+
+    internal static string exchangeType = "fanout";
+
+    internal static int queueExpiration = 5;
+    internal static int prefetchCount = 5;
+    internal static int retryCount = 3;
+    internal static int interval = 5000;
+}
