7738fd47b5879feb03b6e97720788741ae3e9e5c
Angelo Pirola <angelo9871@gmail.com>
2024-04-04 00:04:52 +0200

    Refactoring Messaging Options

--- src/Messaging/GestioneSagre.Messaging/Models/Options/RabbitConsumerOptions.cs
+++ src/Messaging/GestioneSagre.Messaging/Models/Options/RabbitConsumerOptions.cs
@@ -2,13 +2,13 @@
 
 public class RabbitConsumerOptions
 {
-    public string Host { get; set; }
-    public string VirtualHost { get; set; }
-    public string Username { get; set; }
-    public string Password { get; set; }
-    public string ReceivedEndpoint { get; set; }
-    public bool Durable { get; set; }
-    public bool AutoDelete { get; set; }
-    public string ExchangeType { get; set; }
-    public int PrefetchCount { get; set; }
+    //public string Host { get; set; }
+    //public string VirtualHost { get; set; }
+    //public string Username { get; set; }
+    //public string Password { get; set; }
+    //public string ReceivedEndpoint { get; set; }
+    //public bool Durable { get; set; }
+    //public bool AutoDelete { get; set; }
+    //public string ExchangeType { get; set; }
+    //public int PrefetchCount { get; set; }
 }
--- src/Messaging/GestioneSagre.Messaging/Models/Options/RabbitProducerOptions.cs
+++ src/Messaging/GestioneSagre.Messaging/Models/Options/RabbitProducerOptions.cs
@@ -2,16 +2,16 @@
 
 public class RabbitProducerOptions
 {
-    public string Host { get; set; }
-    public string VirtualHost { get; set; }
-    public string Username { get; set; }
-    public string Password { get; set; }
-    public string ReceivedEndpoint { get; set; }
-    public bool Durable { get; set; }
-    public bool AutoDelete { get; set; }
-    public string ExchangeType { get; set; }
-    public int PrefetchCount { get; set; }
-    public int RetryCount { get; set; }
-    public int RetryInterval { get; set; }
-    public double QueueExpiration { get; set; }
+    //public string Host { get; set; }
+    //public string VirtualHost { get; set; }
+    //public string Username { get; set; }
+    //public string Password { get; set; }
+    //public string ReceivedEndpoint { get; set; }
+    //public bool Durable { get; set; }
+    //public bool AutoDelete { get; set; }
+    //public string ExchangeType { get; set; }
+    //public int PrefetchCount { get; set; }
+    //public int RetryCount { get; set; }
+    //public int RetryInterval { get; set; }
+    //public double QueueExpiration { get; set; }
 }
