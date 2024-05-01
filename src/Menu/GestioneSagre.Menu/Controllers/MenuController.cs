72a4b11645f3925765c2d8f7596f27e736fb824f
Angelo Pirola <angelo9871@gmail.com>
2024-05-02 00:01:20 +0200

    Aggiunto API Menu Close #34

---
+++ src/Menu/GestioneSagre.Menu/Controllers/MenuController.cs
@@ -0,0 +1,152 @@
+﻿using GestioneSagre.Menu.BusinessLayer.Services;
+using GestioneSagre.Menu.Controllers.Common;
+using GestioneSagre.Menu.Shared.Models.InputModels;
+using GestioneSagre.Menu.Shared.Models.ViewModels;
+using GestioneSagre.Messaging.Models.InputModels;
+using GestioneSagre.Messaging.Models.ViewModels;
+using GestioneSagre.Shared.OperationResults;
+using GestioneSagre.Shared.OperationResults.WebApi;
+using MassTransit;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GestioneSagre.Menu.Controllers;
+
+public class MenuController : BaseController
+{
+    private readonly IMenuService menuService;
+    private readonly IRequestClient<RequestFestaAttiva> requestClient;
+    private readonly ILogger<MenuController> logger;
+
+    public MenuController(IMenuService menuService, IRequestClient<RequestFestaAttiva> requestClient, ILogger<MenuController> logger)
+    {
+        this.menuService = menuService;
+        this.requestClient = requestClient;
+        this.logger = logger;
+    }
+
+    [HttpGet("ListaMenu")]
+    [ProducesResponseType(typeof(List<MenuViewModel>), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> GetListaProdottiAsync()
+    {
+        try
+        {
+            var result = await menuService.GetListMenuAsync();
+            var response = HttpContext.CreateResponse(result);
+
+            return response;
+        }
+        catch (Exception exc)
+        {
+            return HttpContext.CreateResponse(Result.Fail(FailureReasons.BadRequest, "Richiesta non valida", detail: exc.Message));
+        }
+    }
+
+    [HttpGet("RecoveryIdFesta")]
+    [ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> GetRecoveryIdFestaAsync()
+    {
+        try
+        {
+            Result<string> idFesta = string.Empty;
+
+            using (var request = requestClient.Create(new RequestFestaAttiva { }))
+            {
+                var response = await request.GetResponse<ResponseFestaAttiva>();
+
+                if (response.Message.IdFesta == null)
+                {
+                    logger.LogWarning("Nessun riferimento di festa attiva trovato");
+                    return HttpContext.CreateResponse(Result.Fail(FailureReasons.NotFound, "Nessun dato trovato", "Nessun riferimento di festa attiva trovato"));
+                }
+
+                idFesta = response.Message.IdFesta;
+            }
+
+            var result = HttpContext.CreateResponse(idFesta);
+
+            return result;
+        }
+        catch (Exception exc)
+        {
+            return HttpContext.CreateResponse(Result.Fail(FailureReasons.BadRequest, "Richiesta non valida", detail: exc.Message));
+        }
+    }
+
+    [HttpGet("Portata/{idMenu}/{idFesta}")]
+    [ProducesResponseType(typeof(MenuViewModel), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> GetProdottoAsync(Guid idMenu, Guid idFesta)
+    {
+        try
+        {
+            var result = await menuService.GetMenuAsync(idMenu, idFesta);
+            var response = HttpContext.CreateResponse(result);
+
+            return response;
+        }
+        catch (Exception exc)
+        {
+            return HttpContext.CreateResponse(Result.Fail(FailureReasons.BadRequest, "Richiesta non valida", detail: exc.Message));
+        }
+    }
+
+    [HttpPost("Portata")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> CreateProdottoAsync(MenuCreateInputModel inputModel, CancellationToken cancellationToken = default)
+    {
+        try
+        {
+            var result = await menuService.CreateMenuAsync(inputModel, cancellationToken);
+            var response = HttpContext.CreateResponse(result);
+
+            return response;
+        }
+        catch (Exception exc)
+        {
+            return HttpContext.CreateResponse(Result.Fail(FailureReasons.BadRequest, "Richiesta non valida", detail: exc.Message));
+        }
+    }
+
+    [HttpPut("Portata")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> EditProdottoAsync(MenuEditInputModel inputModel, CancellationToken cancellationToken = default)
+    {
+        try
+        {
+            var result = await menuService.EditMenuAsync(inputModel, cancellationToken);
+            var response = HttpContext.CreateResponse(result);
+
+            return response;
+        }
+        catch (Exception exc)
+        {
+            return HttpContext.CreateResponse(Result.Fail(FailureReasons.BadRequest, "Richiesta non valida", detail: exc.Message));
+        }
+    }
+
+    [HttpDelete("Portata/{idMenu}/{idFesta}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> DeleteProdottoAsync(Guid idMenu, Guid idFesta, CancellationToken cancellationToken = default)
+    {
+        try
+        {
+            var result = await menuService.DeleteMenuAsync(idMenu, idFesta, cancellationToken);
+            var response = HttpContext.CreateResponse(result);
+
+            return response;
+        }
+        catch (Exception exc)
+        {
+            return HttpContext.CreateResponse(Result.Fail(FailureReasons.BadRequest, "Richiesta non valida", detail: exc.Message));
+        }
+    }
+}
---
+++ src/Menu/GestioneSagre.Menu/Startup.cs
@@ -0,0 +1,45 @@
+﻿using GestioneSagre.GenericServices.Extensions;
+using GestioneSagre.Menu.BusinessLayer.Extensions;
+using GestioneSagre.Messaging.Extensions;
+using GestioneSagre.Messaging.Models.InputModels;
+using GestioneSagre.Messaging.Models.Options;
+using GestioneSagre.Shared.Extensions;
+
+namespace GestioneSagre.Menu;
+
+public class Startup
+{
+    private readonly string serviceName = "GestioneSagre.Menu";
+    private readonly string swaggerName = "Gestione Sagre Menu";
+
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers();
+        services.AddRegisterConfigureServices(Configuration, serviceName, swaggerName);
+
+        services.AddDefaultOperationResult();
+        services.AddCustomProblemDetails(Configuration);
+
+        var rabbitOptions = Configuration.GetSection("RabbitMQ").Get<RabbitOptions>();
+        services.AddRabbitMQSender<RequestFestaAttiva>(rabbitOptions);
+    }
+
+    public void Configure(WebApplication app)
+    {
+        IWebHostEnvironment env = app.Environment;
+
+        app.AddCustomConfigure(swaggerName, serviceName);
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+            endpoints.MapHealthChecks();
+        });
+    }
+}
