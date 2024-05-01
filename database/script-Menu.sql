a28614a4f9d865407663d6961681abe5faf99e1f
Angelo Pirola <angelo9871@gmail.com>
2024-05-02 00:01:34 +0200

    Aggiunto script database Menu

---
+++ database/script-Menu.sql
@@ -0,0 +1,29 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+CREATE TABLE [Portate] (
+    [Id] uniqueidentifier NOT NULL,
+    [IdFesta] uniqueidentifier NOT NULL,
+    [IdProdotto] uniqueidentifier NOT NULL,
+    [DataMenu] datetime2 NOT NULL,
+    CONSTRAINT [PK_Portate] PRIMARY KEY ([Id])
+);
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20231228190936_InitialMigration', N'7.0.14');
+GO
+
+COMMIT;
+GO
+
