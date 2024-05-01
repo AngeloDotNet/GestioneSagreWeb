9bc8567158d406cc7bdc54a060f8f08ac74cbab6
Angelo Pirola <angelo9871@gmail.com>
2024-05-01 12:32:25 +0200

    Aggiunti modelli Shared Menu

---
+++ src/Menu/GestioneSagre.Menu.Shared/Models/InputModels/MenuCreateInputModel.cs
@@ -0,0 +1,9 @@
+﻿namespace GestioneSagre.Menu.Shared.Models.InputModels;
+
+public class MenuCreateInputModel
+{
+    public Guid Id { get; set; }
+    public Guid IdFesta { get; set; }
+    public Guid IdProdotto { get; set; }
+    public DateTime DataMenu { get; set; }
+}
---
+++ src/Menu/GestioneSagre.Menu.Shared/Models/ViewModels/MenuViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace GestioneSagre.Menu.Shared.Models.ViewModels;
+
+public class MenuViewModel
+{
+    public Guid Id { get; set; }
+    public Guid IdFesta { get; set; }
+    public Guid IdProdotto { get; set; }
+    public DateTime DataMenu { get; set; }
+}
